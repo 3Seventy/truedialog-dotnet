32ecff5952d0062068b918ba07e7c9c19f2d8f05
ak@truedialog.com <ak@truedialog.com>
2021-08-31 20:46:19 +0300

    - fix long code model

--- TrueDialog.Client.Standard/Model/Channel/LongCode.cs
+++ TrueDialog.Client.Standard/Model/Channel/LongCode.cs
@@ -1,4 +1,6 @@
-﻿namespace TrueDialog.Model
+﻿using System.Runtime.Serialization;
+
+namespace TrueDialog.Model
 {
     /// <summary>
     /// Details for a long code
@@ -8,31 +10,37 @@
         /// <summary>
         /// The account which owns this long code.
         /// </summary>
+        [DataMember]
         public int AccountId { get; set; }
-        
+
         /// <summary>
         /// The channel which handles this long code.
         /// </summary>
+        [DataMember]
         public int ChannelId { get; set; }
-        
+
         /// <summary>
         /// The ANI for this long code.
         /// </summary>
+        [DataMember]
         public string Code { get; set; }
-        
+
         /// <summary>
         /// Call Forwarding number
         /// </summary>
+        [DataMember]
         public string ForwardNumber { get; internal set; }
-        
+
         /// <summary>
         /// Random 6 digit Code that is sent in order to verify forwarding number
         /// </summary>
+        [DataMember]
         public string ForwardVerificationCode { get; internal set; }
-        
+
         /// <summary>
         /// Status of call forwarding
         /// </summary>
+        [DataMember]
         public int? ForwardStatus { get; internal set; }
     }
 }
